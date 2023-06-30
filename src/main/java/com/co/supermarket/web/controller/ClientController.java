9eb875dc2ab48c3a573b8286e9861023884b07e9
León Ángel Chancí Guzmán <leonchanci@gmail.com>
2023-06-29 22:59:02 -0500

    14_crear_documentacion_con_swagger
    Se configura Swagger de manera diferente ya que estoy trabajando con Spring 3.1.1
    http://localhost:8090/supermarket/api/swagger-ui/index.html
    Se corrigen varias cosas para ejecutar el API con PostgreSQL

--- src/main/java/com/co/supermarket/web/controller/ClientController.java
+++ src/main/java/com/co/supermarket/web/controller/ClientController.java
@@ -2,6 +2,7 @@ package com.co.supermarket.web.controller;
 
 import com.co.supermarket.domain.Client;
 import com.co.supermarket.domain.service.ClientService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,11 +17,13 @@ public class ClientController {
     @Autowired
     private ClientService clientService;
 
+    @Operation(summary = "Obtener todos las Clientes")
     @GetMapping("/all")
     public ResponseEntity<List<Client>> getAll(){
         return new ResponseEntity<>(clientService.getALl(), HttpStatus.OK);
     }
 
+    @Operation(summary = "Obtener un Cliente con la Identificación")
     @GetMapping("/{id}")
     public ResponseEntity<Client> getById(@PathVariable("id") String id){
         return clientService.getById(id)
@@ -28,6 +31,7 @@ public class ClientController {
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+    @Operation(summary = "Guardar un objeto Cliente")
     @PostMapping("/save")
     //@RequestBody -> Para determinar que el parámetro es parte del cuerpo de la petición (hace parte del Body->row)
     public ResponseEntity<Client> save(@RequestBody Client client){
